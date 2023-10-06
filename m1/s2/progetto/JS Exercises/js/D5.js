ac3589185c746df24b73fd7efb025dbb372b781b
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-10-06 16:48:36 +0200

    midifica errore

--- m1/s2/progetto/JS Exercises/js/D5.js
+++ m1/s2/progetto/JS Exercises/js/D5.js
@@ -109,6 +109,11 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
 
+let i= 0;
+while (i < numericArray.length && numericArray[i] !== 32) {
+  console.log(numericArray[i]);
+  i++;
+}
 
 
 /* ESERCIZIO 10
