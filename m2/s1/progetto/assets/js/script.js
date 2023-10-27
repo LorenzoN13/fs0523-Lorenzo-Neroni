3a6d90d849cd4566dfcfe7edd33b32578de47462
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-10-27 14:36:30 +0200

    progetto in corso

---
+++ m2/s1/progetto/assets/js/script.js
@@ -0,0 +1,23 @@
+let emme = document.getElementById(m);
+let randomOpacity = Math.random();
+let elementiM = document.querySelector(".elementi-m");
+let navbar = document.querySelector(".navbar")
+let btn = document.querySelector(".btn");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY < 400) {
+        navbar.style.backgroundColor = "#ffc017";
+        btn.style.backgroundColor = "#000";
+    } else {
+        navbar.style.backgroundColor = "#ffff";
+        btn.style.backgroundColor = "#1A8917";
+    }
+});
+
+
+
+function randomM(elemento){
+    emme.style.opacity = randomOpacity;
+}
+
+setInterval(randomM, 2000); 
