0c43fb749c42e842fe0cd5cf06983792b79e50c5
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-11-09 15:57:38 +0100

    in corso

---
+++ m2/s3/g4/js/script.js
@@ -0,0 +1,20 @@
+const apiKey = 'qm7QfLr9hoUJzkQICa7USrV9ieVL3NnRj4nb3Mqo36OWw8MtP6Yv92ep';
+const apiUrl = 'https://api.pexels.com/v1/search?query=pizza';
+
+document.getElementById('loadButton').addEventListener('click', function() {
+  fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${apiKey}`,
+      'Content-Type': 'application/json' 
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+     
+      const card = document.getElementById('imageCard');
+      card.querySelector('.card-title').textContent = data.title;
+      card.querySelector('.card-text').textContent = data.description;
+     
+    })
+});
