b017730b049abed4238cbf67d978278b45eece1e
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-11-10 11:43:36 +0100

    first commit

--- m2/s3/g4/js/script.js
+++ m2/s3/g4/js/script.js
@@ -1,20 +1,31 @@
-const apiKey = 'qm7QfLr9hoUJzkQICa7USrV9ieVL3NnRj4nb3Mqo36OWw8MtP6Yv92ep';
-const apiUrl = 'https://api.pexels.com/v1/search?query=pizza';
-
-document.getElementById('loadButton').addEventListener('click', function() {
-  fetch(apiUrl, {
-    method: 'GET',
+async function getImage(query) {
+  const Url = 'https://api.pexels.com/v1/search?query=';
+  const apiKey = 'qm7QfLr9hoUJzkQICa7USrV9ieVL3NnRj4nb3Mqo36OWw8MtP6Yv92ep';
+  return fetch(Url + query, {
     headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json' 
+      'Content-Type': 'application/json',
+      Authorization: apiKey
     }
+  }).then(res => res.json())
+}
+
+let getImages1 = document.querySelector('#load-image');
+let getImages2 = document.querySelector('#load-secondary');
+ 
+getImages1.addEventListener('click', async () => {
+  let immagini = await getImage('montains')
+  replaceImgs(immagini.photos);
+})
+
+getImages2.addEventListener('click',() => {
+  getImage('pizza').then(res => replaceImgs(res.photos))
+})
+
+
+function replaceImgs(imagesArr) {
+  let cardImages = document.querySelectorAll('.card img')
+
+  cardImages.forEach((img.i) => {
+    img.src = imagesArr[i].src.tiny
   })
-    .then(res => res.json())
-    .then(data => {
-     
-      const card = document.getElementById('imageCard');
-      card.querySelector('.card-title').textContent = data.title;
-      card.querySelector('.card-text').textContent = data.description;
-     
-    })
-});
+}
