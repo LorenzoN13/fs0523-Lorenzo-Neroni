636500bbd1ec49be58bb4f4a730009f330fced8a
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-11-29 14:41:59 +0100

    in corso

---
+++ m3/s2/g3/esercizio/src/app/pages/page1/page1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ICard } from '../../Modules/i-card';
+import { CardService } from '../../card.service';
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.component.html',
+  styleUrl: './page1.component.scss'
+})
+export class Page1Component implements OnInit{
+
+  card:ICard[] = [];
+  constructor(private cardSvc: CardService) {}
+
+    ngOnInit(): void {
+      this.card = this.cardSvc.cards;
+
+}
+
+}
