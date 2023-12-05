552edf313601c19fe193ddc51b572ef102e1c631
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-12-05 15:25:59 +0100

    creazione del form

--- m3/s3/g2/eseercizio/src/app/app.module.ts
+++ m3/s3/g2/eseercizio/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { Page2Component } from './pages/page2/page2.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { Page2Component } from './pages/page2/page2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- m3/s3/g2/eseercizio/src/app/pages/page1/page1.component.ts
+++ m3/s3/g2/eseercizio/src/app/pages/page1/page1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-page1',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Page1Component {
 
+  form!:FormGroup;
+
+  generi:string[] = [
+    "uomo",
+    "donna",
+    "non definito"
+  ];
+
+  constructor(private formBuild:FormBuilder) {}
+
+  ngOnInit(){
+    this.form = this.formBuild.group({
+      nome: this.formBuild.control(null,[Validators.required]),
+      cognome: this.formBuild.control(null,[Validators.required]),
+      autenticazione: this.formBuild.group({
+        username: this.formBuild.control(null,[Validators.required]),
+        password: this.formBuild.control(null,[Validators.required]),
+        confermaPassword: this.formBuild.control(null,[Validators.required])
+      }),
+      genere: this.formBuild.control(null),
+      file: this.formBuild.control(null),
+      textarea: this.formBuild.control(null)
+    });
+    }
 }
